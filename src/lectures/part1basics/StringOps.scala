f26c09f49268ca38c997fb1941acf5def023aaec
Jose Villarreal <>
2021-03-26 15:53:27 -0500

    2.7 String Interpolate

---
+++ src/lectures/part1basics/StringOps.scala
@@ -0,0 +1,40 @@
+package lectures.part1basics
+
+object StringOps extends App {
+
+  val str: String = "Hello, I am learning Scala"
+
+  println(str.charAt(2))
+  println(str.substring(7, 11))
+  println(str.split(" "))
+  println(str.startsWith("Hello"))
+  println(str.replace(" ", "-"))
+  println(str.toLowerCase())
+  println(str.length)
+
+  val aNumberString = "45"
+  val aNumber = aNumberString.toInt
+  println('a' +: aNumberString :+ 'z')
+  println(str.reverse)
+  println(str.take(2))
+
+  // Scala-specific: String interpolators
+
+  // 1. S-interpolators
+  val name = "Thiago"
+  val age = 3
+  val greeting = s"Hello, my name is $name and I am $age years old"
+  val anotherGreeting = s"Hello, my name is $name and I will be turning ${age + 1} years old"
+  println(anotherGreeting)
+
+  // F-Interpolators
+  val speed = 1.2343f
+  val myth = f"$name%s can $speed%2.2f burger per minute"
+  println(myth)
+
+  // raw-interpolator
+  println(raw"This is a \n newline")
+  val escaped = "This is a \n newline"
+  println(raw"$escaped")
+
+}
